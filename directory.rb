080f637a67e4da978b80b5ed116178454728a564
Jake Alvarez <jakealvarez81@hotmail.com>
2015-01-06 00:14:59 +0000

    comments added to directory

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of student 
 puts "The students of my cohort at Makers Academy"
 puts "Gabe"
 puts "..... "
@@ -9,4 +10,6 @@ puts "Luke"
 puts "John"
 puts "etc. etc. etc."
 puts ""
+# finally we print the total. 
 print "Overall, we have\n print 9\n great students"
+# it's important that print does not add new line characters. 
