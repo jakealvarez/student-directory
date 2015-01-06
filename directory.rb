55c7c3ef32eb78134393f6213a01575b34243799
Jake Alvarez <jakealvarez81@hotmail.com>
2015-01-06 00:22:05 +0000

    added the student count

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 # first we print the list of student 
+student_count = 9
 puts "The students of my cohort at Makers Academy"
 puts "Gabe"
 puts "..... "
@@ -11,5 +12,5 @@ puts "John"
 puts "etc. etc. etc."
 puts ""
 # finally we print the total. 
-print "Overall, we have\n print 9\n great students"
+puts "Overall, we have #{student_count} great students"
 # it's important that print does not add new line characters. 
