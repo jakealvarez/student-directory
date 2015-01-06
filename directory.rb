adbbc1888026cdd89764d5cc8933110a895d15e8
Jake Alvarez <jakealvarez81@hotmail.com>
2015-01-06 02:50:30 +0000

    taking the array and printing the values separately

--- directory.rb
+++ directory.rb
@@ -11,14 +11,6 @@ puts "To finish, just hit enter twice."
 students = []
 #get the name of the first student 
 name = gets.chomp
-#  Firstly, we create an empty array to store our future students 
-#(line 5). Then we ask for the name of the first student (line 7) 
-#and discard the return character. Then, we write a block of code 
-#(lines 9 to 15) that will be executed repeatedly as long as the name 
-#variable is not empty (the "empty?" method is part of the String 
-#class). If the name the user entered on line 7 is not empty 
-#(exclamation mark at the beginning means negation), the block will 
-# be executed at least once.
 while !name.empty? do 
 # add the student has to the array. 
   students << {:name => name, :cohort => :november}
@@ -29,6 +21,13 @@ end
 #this is importants so that the method output will return the array of students
 students
 end
+
+# the students array is outputed from input_students and will print as an array, not its contents, so... 
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end 
 
 def print_footer(names)
   puts ""
