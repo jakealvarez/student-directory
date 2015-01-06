701cd20d60824bc52b5b599ecc051c99d457ef9c
Jake Alvarez <jakealvarez81@hotmail.com>
2015-01-06 00:46:41 +0000

    used the .each method on the array to pass each element to a block which puts the student name

--- directory.rb
+++ directory.rb
@@ -7,16 +7,17 @@ students = [
   "Matthew",
   "Mark",
   "Luke",
-  "John"
+  "John",
+  "Samantha"
 ]
-# and then print out each element of the array. 
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[6]
+# instead of printing out each element of the array (puts students[0] etc.)
+# , we can iterate.... The .each method takes the first element of the array and passes it 
+# to the variable name between the pipes. The block then executes the code on the variable in the pipes. 
+# next, when the block has finished executing, the .each method passes the next element in the array to the variable in the block. 
+
+students.each do |student|
+  puts student
+end
+
 #finally, we print the total
 puts "Overall, we have #{students.length} great students"
